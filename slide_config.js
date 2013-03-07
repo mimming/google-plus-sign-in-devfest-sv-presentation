c9b328d97aaedcdb2039afbcf542c80eb222cf0d
Jonathan Beri <jmberi@gmail.com>
2013-03-07 11:08:48 -0800

    added basic configs

--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'Introducing Google+ Sign-In',
+    subtitle: 'See what is possible in your App with the Google+ social layer',
     //eventTitle: 'Google I/O 2013',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
@@ -19,12 +19,11 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Jonathan Beri',
+    company: 'Developer Advocate, Google+',
+    gplus: 'http://www.google.com/+JonathanBeri',
+    www: 'http://www.jonathanberi.com',
+    github: 'http://github.com/beriberikix'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
